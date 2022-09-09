6c6b05c880ba2b2c17a17b83be9a2b62afc45176
Bruno <brunolutterbach13@gmail.com>
2022-09-08 21:14:15 -0300

    Exception

---
+++ src/main/java/br/com/brunolutterbach/alurachallengebackend/exceptions/ValidacaoException.java
@@ -0,0 +1,9 @@
+package br.com.brunolutterbach.alurachallengebackend.exceptions;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+
+}
+
